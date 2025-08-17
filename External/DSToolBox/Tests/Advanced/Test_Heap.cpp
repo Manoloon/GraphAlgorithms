9cf8eeb376b6870bfcbd0c3365f7476907154684
Pablo Sanchez <pablo.r.sanchez@hotmail.com>
2025-08-17 11:11:02 +0200

    tests with Fixture

--- External/DSToolBox/Tests/Advanced/Test_Heap.cpp
+++ External/DSToolBox/Tests/Advanced/Test_Heap.cpp
@@ -4,23 +4,40 @@ extern "C"
 {
     #include "../Advanced/Heap.h"
 }
-heap_t* heap = NULL;
-TEST(DSTB_Heap,CreateEmptyMin_MinCapacity)
+class DSTB_Heap : public ::testing::Test
 {
-    heap = heap_create_min(10);
+    protected:
+        heap_t* heap;
+        WeightedNode_t nod1 = {0.1f,10};
+        WeightedNode_t nod2 = {0.2f,20};
+        WeightedNode_t nod3 = {0.7f,70};
+        WeightedNode_t nod4 = {0.4f,40};
+    void SetUp() override
+    {
+        heap = heap_create_min(10);
+    }
+    void TearDown() override
+    {
+        // destroy heap
+    }
+};
+TEST_F(DSTB_Heap,CreateMinEmpty)
+{
     ASSERT_NE(heap,nullptr);
     EXPECT_EQ(heap_is_empty(heap),true);
+    ASSERT_TRUE(heap_is_min(heap));
 }
-TEST(DSTB_Heap,Insert)
+TEST_F(DSTB_Heap,Insert)
 {
-    heap_insert(heap,(WeightedNode_t){0.1f,10});
-    heap_insert(heap,(WeightedNode_t){0.7f,7});
-    heap_insert(heap,(WeightedNode_t){0.3f,30});
-    ASSERT_EQ(heap_is_empty(heap),false);
+    heap_insert(heap,nod2);
+    heap_insert(heap,nod3);
+    heap_insert(heap,nod4);
+    ASSERT_FALSE(heap_is_empty(heap));
 }
-TEST(DSTB_Heap,Extract)
+TEST_F(DSTB_Heap,Extract)
 {
+    heap_insert(heap,nod1);
     WeightedNode_t ext = heap_extract(heap);
-    ASSERT_EQ(ext.weight,0.1f);
-    EXPECT_EQ(ext.data,10);
+    ASSERT_EQ(ext.weight,nod1.weight);
+    EXPECT_EQ(ext.data,nod1.data);
 }
--- External/DSToolBox/Tests/Linear/Test_SimpleQueue.cpp
+++ External/DSToolBox/Tests/Linear/Test_SimpleQueue.cpp
@@ -4,15 +4,50 @@ extern "C"
 {
     #include "../Linear/SimpleQueue.h"
 }
-queue_t* q = NULL;
-TEST(DSTB_Queue,CreateEmpty)
+// fixture
+class DSTB_SimpleQueue : public ::testing::Test
 {
-    q = queue_create(4);
+    protected:
+        queue_t* q;
+        int val1 = 10;
+        int val2 = 20;
+        int val3 = 30;
+        int val4 = 40;
+        void SetUp() override
+        {
+            q = queue_create(10);
+            queue_push(q, &val1);
+            queue_push(q, &val2);
+            queue_push(q, &val3);
+            queue_push(q, &val4);
+        }
+        void TearDown() override
+        {
+            queue_destroy(q);
+        }
+};
+
+TEST_F(DSTB_SimpleQueue,Created)
+{
     ASSERT_NE(q,nullptr);
 }
 
-TEST(DSTB_Queue,DestroyIt)
+TEST_F(DSTB_SimpleQueue,Push)
 {
-    queue_destroy(q);
-    ASSERT_EQ(q,nullptr);
+    EXPECT_FALSE(queue_is_empty(q));
+    int* peek =(int*) queue_peek(q);
+    ASSERT_EQ(10,*peek);
+}
+
+TEST_F(DSTB_SimpleQueue,Pop)
+{
+    int* pop = (int*)queue_pop(q);
+    EXPECT_EQ(10,*pop);
+    pop = (int*)queue_pop(q);
+    EXPECT_EQ(20,*pop);
+    pop = (int*)queue_pop(q);
+    EXPECT_EQ(30,*pop);
+    pop = (int*)queue_pop(q);
+    EXPECT_EQ(40,*pop);
+    EXPECT_TRUE(queue_is_empty(q));
 }
